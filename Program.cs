866f94b0ed34645d4010d9a1ed045c657f11cdfc
Mônica <monicambs@1@gmail.com>
2022-09-19 16:25:29 -0300

    Resolução Exercício

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.Write("Digite seu nome......!");
+nome = Console.ReadLine();
+
+Console.Write("Digite seu sobrenome......!");
+sobrenome = Console.ReadLine();
+
+Console.WriteLine($"{nome} {sobrenome}");
+Console.WriteLine($"{sobrenome.ToUpper()},{nome}");
